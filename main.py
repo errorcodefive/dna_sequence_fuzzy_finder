29c94293d79a7f39ef22ef29c728786dc5620c7a
Kyle Chin <kylechin@errorcodefive.com>
2020-02-09 10:49:42 -0500

    add config parsing, file checker

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# Using GRCh38 File
+import numpy as np
+import configparser
 
+# List of .fasta files to go through, I have them named chr1-22, chrx, chry
+# Files must be placed in same directory as main.py
+chr_list = [
+    "chr1", 
+    "chr2"
+    ]
+# Only loading 2 chromosomes during testing
 
 # Load parameters from config.ini file
+config = configparser.ConfigParser()
+config.read('config.ini')
+seq=config['DEFAULT']['sequence']
+surround_len=config['DEFAULT']['surround_length']
 
-# Check for all chromosome files
+print ("Sequence: " + seq +". Len: " + surround_len)
+# Check chromosome files in chr_list are present
 
-#Iterate through each chromosome
+for chr in chr_list:
+    try:
+        f=open(chr+".fasta")
+    except FileNotFoundError:
+        print("File: " + chr+".fasta"+" not found.")
+    finally:
+        f.close()
+# Iterate through each chromosome
 
 # Iterate through every nucleotide and location for sequence
 
 # If sequence is found then note: location, chromosome, +- 50nt sequence
 
-
+# Write to file as you go in case of errors
 
 # Use GFF after Loci are found to find annotation information
