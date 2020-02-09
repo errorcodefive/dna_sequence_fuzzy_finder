851a9ecdf08f18121334d7f825bf5d5a5f2fb73b
Kyle Chin <kylechin@errorcodefive.com>
2020-02-09 16:16:01 -0500

    able to find sequence but numbering is not correct, test.fasta is in drive

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import numpy as np
 import configparser
+import time
+from collections import deque
 
 # List of .fasta files to go through, I have them named chr1-22, chrx, chry
 # Files must be placed in same directory as main.py
-chr_list = [
-    "chr1", 
-    "chr2"
+chr_list = ["test"
     ]
 # Only loading 2 chromosomes during testing
 
@@ -13,9 +13,9 @@ chr_list = [
 config = configparser.ConfigParser()
 config.read('config.ini')
 seq=config['DEFAULT']['sequence']
-surround_len=config['DEFAULT']['surround_length']
+surround_len=int(config['DEFAULT']['surround_length'])
 
-print ("Sequence: " + seq +". Len: " + surround_len)
+print ("Sequence: " + seq +". Len: " + str(surround_len))
 # Check chromosome files in chr_list are present
 
 for chr in chr_list:
@@ -27,6 +27,44 @@ for chr in chr_list:
         f.close()
 # Iterate through each chromosome
 
+for chr in chr_list:
+    # Open chr file
+    with open(chr+'.fasta') as infile:
+        print("First" + infile.readline())
+        
+        line_first = ""
+        line_last = ""
+        line_combined = ""
+        total_char = 0
+        line_num_char = 0
+        full_pop_size = len(seq)+surround_len*2
+        frame_queue = deque([])
+        for new_line in infile:
+            for nuc in new_line.strip():
+                total_char +=1
+                # If frame_queue is not fully populated
+                if len(frame_queue)<full_pop_size:
+                    frame_queue.append(nuc)
+                else:
+                    frame_queue.append(nuc)
+                    frame_queue.popleft()
+                    # Check for seq
+                    seq_match=True
+                    frame_pos = int(surround_len)
+                    seq_pos = 0
+                    while seq_match == True:
+                        #Look at specific position
+                        if frame_queue[frame_pos] == seq[seq_pos]:
+                            seq_match = True
+                            seq_pos+=1
+                            frame_pos+=1
+                        else:
+                            seq_match = False
+                        if(seq_pos==len(seq)):
+                            print("Sequence found at " + str(total_char-surround_len+2-len(seq))+ ".")
+                            seq_match = False
+                            #Save position and surrounding sequences
+        print("Finished and process " + str(total_char) + " nucleotides.")
 # Iterate through every nucleotide and location for sequence
 
 # If sequence is found then note: location, chromosome, +- 50nt sequence
