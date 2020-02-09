20e6963aeb3067d249749056405af34dd3adf99b
Kyle Chin <kylechin@errorcodefive.com>
2020-02-08 22:20:02 -0500

    more pseudo code added

--- main.py
+++ main.py
@@ -2,3 +2,15 @@
 
 
 # Load parameters from config.ini file
+
+# Check for all chromosome files
+
+#Iterate through each chromosome
+
+# Iterate through every nucleotide and location for sequence
+
+# If sequence is found then note: location, chromosome, +- 50nt sequence
+
+
+
+# Use GFF after Loci are found to find annotation information
