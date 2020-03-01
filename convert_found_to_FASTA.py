917f51748924403a35aeb1230da9df96650fc948
Kyle Chin <errorcodefive@gmail.com>
2020-02-29 21:45:46 -0500

    added description for convert_found_to_FASTA

---
+++ convert_found_to_FASTA.py
@@ -0,0 +1,3 @@
+# convert the found_sequence files to a separate .FASTA sequence
+# 80 Character width
+# ">" before description line, use the sequence nucleotide number, sense/antisense and file name in description
