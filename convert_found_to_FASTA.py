50998d1a6f8795a84011046b00ee6777b6ae8841
Kyle Chin <kylechin@errorcodefive.com>
2020-03-06 19:19:19 -0500

    pseudo code

--- convert_found_to_FASTA.py
+++ convert_found_to_FASTA.py
@@ -1,3 +1,10 @@
 # convert the found_sequence files to a separate .FASTA sequence
 # 80 Character width
 # ">" before description line, use the sequence nucleotide number, sense/antisense and file name in description
+
+#load found_sequence_anti_fixed
+#load found_sequence
+# Create output file
+
+# While not eol -> get line
+# 
